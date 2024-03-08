6f81c3c4e5e2675ddc7cffcd721eccfc5f755aff
jgonzalez-stw <jgonzalez-stw@users.noreply.github.com>
2024-03-08 18:37:26 +0000

    deploy: 5345c4e23bbb1564be755f233156e082ad54bca8

---
+++ 1.2.1/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['autoloadlocalizer_0',['AutoLoadLocalizer',['../classI18N_1_1DotNet_1_1AutoLoadLocalizer.html#ae0c4a3234fe720385dcf5031f297c487',1,'I18N.DotNet.AutoLoadLocalizer.AutoLoadLocalizer()'],['../classI18N_1_1DotNet_1_1AutoLoadLocalizer.html',1,'AutoLoadLocalizer']]]
+];
---
+++ 1.2.1/search/properties_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['localizer_0',['Localizer',['../classI18N_1_1DotNet_1_1GlobalLocalizer.html#a934513e706fa695136a1146ca188478a',1,'I18N::DotNet::GlobalLocalizer']]]
+];

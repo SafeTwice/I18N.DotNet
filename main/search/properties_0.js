99ca3da99af0ea0594c7d177adf6b59967ebde7c
jgonzalez-stw <jgonzalez-stw@users.noreply.github.com>
2024-03-08 13:53:19 +0000

    deploy: a7e0dae6910e42958a75725990bc17639e5aebf7

--- main/search/properties_0.js
+++ main/search/properties_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['localizer_0',['Localizer',['../classI18N_1_1DotNet_1_1GlobalLocalizer.html#a89b6c75f72a21cc0851fc6eeacf8c414',1,'I18N::DotNet::GlobalLocalizer']]]
+  ['localizer_0',['Localizer',['../classI18N_1_1DotNet_1_1GlobalLocalizer.html#a934513e706fa695136a1146ca188478a',1,'I18N::DotNet::GlobalLocalizer']]]
 ];
